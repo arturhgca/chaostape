e52571a78cf400294a308ac55a9ac393bd7b3fe2
arturhgca <arturhgca@gmail.com>
2018-11-27 15:22:26 -0200

    Add YouTube playlist module

---
+++ chaostape/youtube.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from discord import Channel
+
+
+def create_playlist():
+    # TODO
+    pass
+
+
+def add_to_playlist(channel: Channel, links: List[str]):
+    # TODO
+    pass
+
+
+def update_playlist_name():
+    # TODO
+    pass
